5cd2d99f623bc83e65ab5158998da5c3fe169088
davMac606 <cl201238@g.unicamp.br>
2024-02-28 06:52:49 -0300

    Remade exercises using functions and verifying input type.

--- list.py
+++ list.py
@@ -1,32 +1,111 @@
+def error():
+    print("Invalid input. Please enter a number.")
+    exit()
+
+
 print("This program was developed by Davi Andrade Macedo.")
 print("I could do multiple files for this, but I'd rather do it all here.")
-print("Exercise 1: C to F converter.")
 
-CInput=input("Enter the temperature in C°: ")
 
-CTemp = float(CInput);
+print("\n\nExercise 1: °C to °F converter.")
 
-FTemp = 9*CTemp/5+32
+CInput=input("Enter the temperature in °C: ")
+try:
+    CTemp = float(CInput);
+    FTemp = 9*CTemp/5+32
+    print("The temperature in °F is: ", FTemp, sep="")
 
-print("The temperature in F° is: ", FTemp, sep="")
+except:
+    error()
+    print("\n\nExercise 2: °F to °C converter.")
 
-print("Exercise 2: F to C converter.")
-FInput = input("Enter the temperature in F°: ")
 
-FTemp = float(FInput)
+FInput = input("Enter the temperature in °F: ")
+try:
+    FTemp = float(FInput)
+    CTemp = (FTemp-32)*5/9
+    print("The temperature in °C is: ", CTemp, sep="")
+except:
+    error()
 
-CTemp = (FTemp-32)*5/9
 
-print("The temperature in C° is: ", CTemp, sep="")
+print("\n\nExercise 3:  °C to °K converter.")
+CInput = input("Enter the temperature in °C: ")
+try:
+    CTemp = float(CInput)
+    KTemp = CTemp + 273.15
+    print("The temperature in °K is: ", KTemp, sep="")
+except:
+    error()
 
-print("Exercise 3:  C to K converter.")
-CInput = input("Enter the temperature in C°: ")
-CTemp = float(CInput)
-KTemp = CTemp + 273.15
-print("The temperature in K is: ", KTemp, sep="")
 
-print("Exercise 4: K to C converter.")
-KInput = input("Enter the temperature in K: ")
-KTemp = float(KInput)
-CTemp = KTemp - 273.15
-print("The temperature in C° is: ", CTemp, sep="")
+print("\n\nExercise 4: °K to °C converter.")
+KInput = input("Enter the temperature in °K: ")
+try:
+    KTemp = float(KInput)
+    CTemp = KTemp - 273.15
+    print("The temperature in °C is: ", CTemp, sep="")
+except:
+    error()
+
+
+print("\n\nExercise 5: °C to °R converter.")
+CInput = input("Enter the temperature in °C: ")
+try:
+    CTemp = float(CInput)
+    RaTemp = (CTemp/1.8) - 491.67
+    print("The temperature in °R is: ", RaTemp, sep="")
+except:
+    error()
+
+print("\n\nExercise 6: °R to °C converter.")
+RInput = input("Enter the temperature in °R: ")
+try:
+    RTemp = float(RInput)
+    CTemp = (RaTemp + 491.67)*1.8
+    print("The temperature in °C is: ", CTemp, sep="")
+except:
+    error()
+
+
+print("\n\nExercise 7: F to K converter.")
+print("So, there are two ways to convert Fahrenheit to Kelvin: We can get it from F>C>K or F>K. I'll be using F>C>K.")
+FInput = input("Enter the temperature in °F: ")
+try:
+    FTemp = float(FInput)
+    CTemp = (FTemp-32)*5/9
+    KTemp = CTemp + 273.15
+    print("The temperature in °K is: ", KTemp, sep="")
+except:
+    error()
+
+
+print("\n\nExercise 8: °K to °F converter.")
+print("This time, I'll be using K>F directly.")
+KInput = input("Enter the temperature in °K: ")
+try:
+    KTemp = float(KInput)
+    FTemp = 1.8*(KTemp - 273.15) + 32
+    print("The temperature in °F is: ", FTemp, sep="")  
+except:
+    error()
+
+print("\n\nExercise 9: °F to °R converter.")
+FInput = input("Enter the temperature in °F: ")
+try:
+    FTemp = float(FInput)
+    RaTemp = (FTemp/1.8) - 491.67
+    print("The temperature in °R is: ", RaTemp, sep="")
+except:
+    error()
+
+print("\n\nExercise 10: °R to °F converter.")
+RInput = input("Enter the temperature in °R: ")
+try:
+    RTemp = float(RInput)
+    FTemp = RTemp - 459.67
+    print("The temperature in °F is: ", FTemp, sep="")
+except:
+    error()
+
+
