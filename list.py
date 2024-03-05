033b71826a89d57d9069016699e136352e67ab45
davMac606 <cl201238@g.unicamp.br>
2024-03-04 22:14:20 -0300

    forgot i had removed past exercises to more easily test.

--- list.py
+++ list.py
@@ -5,6 +5,156 @@ def ValueErrorEx():
 def ZeroInputEx():
     print("Invalid input. Please enter a number different from zero.")
 
+print("This program was developed by Davi Andrade Macedo.")
+print("I could do multiple files for this, but I'd rather do it all here.")
+
+print("\n\nExercise 1: °C to °F converter.")
+while True:
+    CInput=input("Enter the temperature in °C: ")
+    try:
+        CTemp = float(CInput);
+        FTemp = 9*CTemp/5+32
+        print("The temperature in °F is: ", FTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
+   
+
+print("\n\nExercise 2: °F to °C converter.")
+while True: 
+    FInput = input("Enter the temperature in °F: ")
+    try:
+        FTemp = float(FInput)
+        CTemp = (FTemp-32)*5/9
+        print("The temperature in °C is: ", CTemp, sep="")
+        break   
+    except:
+        ValueErrorEx()
+
+
+print("\n\nExercise 3:  °C to °K converter.")
+while True:
+    CInput = input("Enter the temperature in °C: ")
+    try:
+        CTemp = float(CInput)
+        KTemp = CTemp + 273.15
+        print("The temperature in °K is: ", KTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
+
+
+
+print("\n\nExercise 4: °K to °C converter.")
+while True:
+    KInput = input("Enter the temperature in °K: ")
+    try:
+        KTemp = float(KInput)
+        CTemp = KTemp - 273.15
+        print("The temperature in °C is: ", CTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
+
+
+
+print("\n\nExercise 5: °C to °R converter.")
+while True:
+    CInput = input("Enter the temperature in °C: ")
+    try:
+        CTemp = float(CInput)
+        RTemp = CTemp*9/5 + 491.67
+        print("The temperature in °R is: ", RTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
+
+
+print("\n\nExercise 6: °R to °C converter.")
+while True:
+    RInput = input("Enter the temperature in °R: ")
+    try:
+        RTemp = float(RInput)
+        CTemp = (RTemp - 491.67)/1.8
+        print("The temperature in °C is: ", CTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
+
+
+print("\n\nExercise 7: F to K converter.")
+while True:
+    FInput = input("Enter the temperature in °F: ")
+    try:
+        FTemp = float(FInput)
+        CTemp = (FTemp-32)*5/9
+        KTemp = CTemp + 273.15
+        print("The temperature in °K is: ", KTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
+
+
+print("\n\nExercise 8: °K to °F converter.")
+while True:
+    KInput = input("Enter the temperature in °K: ")
+    try:
+        KTemp = float(KInput)
+        CTemp = KTemp - 273.15
+        FTemp = 9*CTemp/5+32
+        print("The temperature in °F is: ", FTemp, sep="")  
+        break
+    except:
+        ValueErrorEx()
+
+print("\n\nExercise 9: °F to °R converter.")
+while True:
+    FInput = input("Enter the temperature in °F: ")
+    try:
+        FTemp = float(FInput)
+        CTemp = (FTemp-32)*5/9
+        RTemp = CTemp*9/5 + 491.67
+        print("The temperature in °R is: ", RTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
+
+print("\n\nExercise 10: °R to °F converter.")
+while True:
+    RInput = input("Enter the temperature in °R: ")
+    try:
+        RTemp = float(RInput)
+        CTemp = (RTemp - 491.67)/1.8
+        FTemp = RTemp - 459.67  
+        print("The temperature in °F is: ", FTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
+
+
+print("\n\nExercise 11: °K to °R converter.")
+while True:
+    KInput = input("Enter the temperature in °K: ")
+    try:
+        KTemp = float(KInput)
+        CTemp = KTemp - 273.15
+        RTemp = CTemp*9/5 + 491.67
+        print("The temperature in °R is: ", RTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
+
+print("\n\nExercise 12: °R to °K converter.")
+while True:
+    RInput = input("Enter the temperature in °R: ")
+    try:
+        RTemp = float(RInput)
+        CTemp = (RTemp - 491.67)/1.8
+        KTemp = CTemp + 273.15
+        print("The temperature in °K is: ", KTemp, sep="")
+        break
+    except:
+        ValueErrorEx()
 
 print("\n\nExercise 13: Perimeter of a triangle.")
 while True:  
