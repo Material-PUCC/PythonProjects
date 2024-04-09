75590a41f88b3481526b6bc1e79a5b8e27d3a00e
davMac606 <worksender053@gmail.com>
2024-04-09 20:46:43 -0300

    Added new ways of solving ex3 and ex4 from list exercises
    Ways accepted by the teacher(he's too silly)

--- minmax.py
+++ minmax.py
@@ -20,7 +20,7 @@ def ex3():
             for num in list1:
                 if num > highest:
                     highest = num
-            print("The highest number is ", highest)
+            print("The highest number is", highest, "with index", list1.index(highest), ".")
             break
         except ValueError:
             main.ValueErrorEx()
@@ -43,6 +43,30 @@ def ex3():
 def ex4():
     while True:
         try:
+            list1 = []
+            i = 1
+            times = int(input("\n\nEnter how many numbers you want in the list: "))
+            
+            while i <= times:
+                while True:
+                    try:
+                        num = int(input("Insert a number to the list: "))
+                        break
+                    except ValueError:
+                        main.ValueErrorEx()
+                list1.append(num)
+                i = i + 1
+            lowest = list1[0]
+            for num in list1:
+                if num < lowest:
+                    lowest = num
+            print("The lowest number is", lowest, "with index", list1.index(lowest), ".")
+            break
+        except ValueError:
+            main.ValueErrorEx()
+    '''
+    while True:
+        try:
             i = 0
             times = int(input("\n\nEnter how many elements the list will have: "))
             while i < times:
@@ -53,3 +77,4 @@ def ex4():
             print(str(min(list1)))
         except ValueError:
             main.ValueErrorEx()
+            '''
